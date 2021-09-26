705aada146f57e8575e131ecafa97b9a52d8913a
mathcodeman <zongzhen@ualberta.ca>
2021-09-26 11:44:52 -0700

    update

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ storage = {}
 @app.route("/",methods=["POST","GET"])
 def main():
     if request.method=="POST":
-        url = request.form.get("yt_url")
-        yt = YouTube(url)
-        ID = yt.video_id
-        storage["ytObject"] = yt
-        return redirect(f"/downloadPage/{ID}")
+        try:
+            url = request.form.get("yt_url")
+            yt = YouTube(url)
+            ID = yt.video_id
+            storage["ytObject"] = yt
+            return redirect(f"/downloadPage/{ID}")
+        except:
+            msg = "Please enter a valid URL"
+            return render_template("home.html",msg=msg)
     else:
         return render_template("home.html")
 
@@ -25,19 +29,27 @@ def downloadPage(video_url):
     audio_streams = yt.streams.filter(only_audio=True)
     #mp4 download
     video_streams = yt.streams.filter(file_extension="mp4")
-    return render_template("downloadPage.html",title=title,videoID=videoID,audio_streams=audio_streams,video_streams=video_streams)
+    #caption download
+    caption_choices = yt.captions.lang_code_index
+    return render_template("downloadPage.html",title=title,videoID=videoID,
+                            audio_streams=audio_streams,video_streams=video_streams,
+                            caption_choices=caption_choices)
 
 @app.route("/download",methods=["POST","GET"])
 def download():
     youtube_object = storage["ytObject"]
     audio_downloadTag = request.form.get("audios")
     video_downloadTag = request.form.get("videos")
+    caption_downloadTag = request.form.get("captions")
     if audio_downloadTag:
         file_path = youtube_object.streams.get_by_itag(audio_downloadTag).download()
         return send_file(file_path,as_attachment=True)
     elif video_downloadTag:
         file_path = youtube_object.streams.get_by_itag(video_downloadTag).download()
         return send_file(file_path,as_attachment=True)
+    elif caption_downloadTag:
+        caption_raw = youtube_object.captions.get_by_language_code(caption_downloadTag)
+        return caption_raw.xml_captions
 
 @app.route("/search", methods=["POST","GET"])
 def search():
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-from re import S
-from pytube import YouTube, Search
-import xmltodict, json
-
-S = Search("baby")
-for i in S.results:
-    print(i.video_id)
+from pytube import YouTube, Search, captions
 
 
+yt = YouTube("http://youtube.com/watch?v=2lAe1cqCOXo")
+choice = yt.captions
 
+print(choice.lang_code_index)
+for k,v in choice.lang_code_index.items():
+    print(k)
+    caption = choice.get_by_language_code(k)
+    print(caption.xml_captions)
